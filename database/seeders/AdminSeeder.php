e260e6da47b10f041fd2ff000167d4b7d266b9a8
kamrulWSIT <kamrul@workspaceit.com>
2024-12-17 09:07:20 +0600

    create role and user seeder

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +15,13 @@ class AdminSeeder extends Seeder
      */
     public function run()
     {
-        Role::create(['name' => 'writer']);
+        $user = User::create([
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        ]);
+
+        $user->assignRole('writer', 'admin');
     }
 }
